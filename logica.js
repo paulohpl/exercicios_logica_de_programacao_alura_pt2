3381da0fc175cb48099d8f5e56f0f68232e48e01
Paulo <paulo.ipa9@gmail.com>
2024-01-18 15:03:03 -0300

    jogo numero secreto completo

---
+++ logica.js
@@ -0,0 +1,22 @@
+alert('Bem vindo ao jogo do numero secreto');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao numero secreto
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e 10`);
+    // se chute for igual ao número secreto 
+    if (chute == numeroSecreto) {
+        alert (`Isso ai você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert (`O número secreto é menor que ${chute}`);
+        } else {
+            alert (`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;
+    } 
+    }
